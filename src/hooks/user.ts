28379d641dab0ba99afda190d2d60dac3e94d933
godotdotdot <chukui0202@gmail.com>
2021-01-31 16:26:29 +0800

    chore: 修改部署文件

--- src/hooks/user.ts
+++ src/hooks/user.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { isConnected, getAccount } from '@/service/metamask'
 import store from '@/common/store';
 
